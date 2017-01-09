2073a2fa9d43ab50c6e5adea89d5eedf2bd22858
nokizorque <noki@zorque.xyz>
2017-01-09 13:13:15 +1000

    API is now properly RESTful and uses route handling

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var bodyParser 	= require("body-parser");
 var port 		= process.env.PORT || 3000;
 var router 		= express.Router();
 
+var User 		= require("./models/users");
+
 // Allows us to pull data from POST requests
 // And allows us to use JSON in our response
 // Automatically adjusts our response headers to 'application/json'
@@ -18,6 +20,25 @@ router.use(function(req, res, next) {
 router.get("/", function (req, res) {
 	res.json({message: "api.twenti.co"});
 });
+
+router.route("/users")
+	.post(function(req, res) {
+		res.json({message: "post"});
+	})
+	.get(function(req, res) {
+		res.json({message: "get"});
+	})
+	.put(function(req, res) {
+		res.json({message: "put"});
+	})
+	.delete(function(req, res) {
+		res.json({message: "delete"});
+	});
+
+router.route("/users/:name")
+	.get(function(req, res) {
+		res.json({message: "get " + req.params.name});
+	})
 
 app.use("/", router);
 app.listen(port);
