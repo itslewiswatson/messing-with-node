dd2cd673efe81ade1ca0284469ba57d0399a8855
nokizorque <noki@zorque.xyz>
2017-01-16 16:31:04 +1000

    Array helpers added

---
+++ helpers/array.js
@@ -0,0 +1,13 @@
+module.exports.removeDuplicates = function(a) {
+   return Array.from(new Set(a));
+}
+
+Array.prototype.clean = function(deleteValue) {
+	for (var i = 0; i < this.length; i++) {
+		if (this[i] == deleteValue) {         
+			this.splice(i, 1);
+			i--;
+		}
+	}
+	return this;
+};
