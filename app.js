29b07f4ebed7951bf23476d4d6964ae03ff34e35
nokizorque <noki@zorque.xyz>
2017-01-16 16:32:27 +1000

    Removed direct interaction with the database from the main route file (app.js)

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var bodyParser		= require("body-parser");
 var port 			= process.env.PORT || 3000;
 var router			= express.Router();
 var randomString 	= require("randomstring");
-var db				= require("./models/postgre.js");
+var tracks			= require("./controllers/tracks.js");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -39,7 +39,11 @@ router.route("/users")
 		);
 	})
 	.get(function(req, res) {
-		res.json({message: "get"});
+		res.json(
+			{
+				message: "get"
+			}
+		);
 	})
 	.put(function(req, res) {
 		res.json({message: "put"});
@@ -51,43 +55,23 @@ router.route("/users")
 router.route("/users/:name")
 	.get(function(req, res) {
 		//res.json({message: "get " + req.params.name});
-	})
+	});
+
+router.route("/tracks")
+	.get(function(req, res) {
+		res.json({message: "get"});
+	});
 
 router.route("/tracks/:id")
 	.get(function(req, res) {
-		var id = req.params.id;
-		db.one("select * from tracks where uniqueid = $1", id)
-			.then(function (data) {
-				// Idea
-					// Currently integer fields return as string in the json response
-					// Loop through all fields
-					// Check if parseInt(field) returns a number
-					// If it does, set the field's value to parseInt of it's value
-					/*
-						if (parseInt(field)) {
-							field = parseInt(field);
-						}
-					*/
-					// Should see if it's able to be made into a one-liner
-				// eg: data.uploaded = parseInt(data.uploaded);
-				res.status(200)
-					.json(
-						{
-							"response": {
-								"data": data
-							}
-						}
-					);
-			})
-			.catch(function (err) {
-				res.json(
-					{
-						"response": {
-							"data": null
-						}
-					}
-				);
-			});
+		var ids = req.params.id;
+		var split = ids.split(",");
+		if (split && split.length && split.length > 1) {
+			tracks.getMany(req, res);
+		}
+		else {
+			tracks.getSingle(req, res);
+		}
 	});
 
 app.use("/", router);
