1099196a5881de841d7339feacf5bf7ac0cf575f
nokizorque <noki@zorque.xyz>
2017-01-16 23:08:57 +1000

    Added JSON helper file

---
+++ helpers/json.js
@@ -0,0 +1,22 @@
+/*
+// Copyright (C) 2016 - 2017 Lewis Watson <noki@zorque.xyz>
+//
+// This file is part of Twenti
+//
+// Twenti can not be copied and/or distributed without the express
+// permission of Lewis Watson and/or Franco Paez
+*/
+
+function extend(target) {
+    var sources = [].slice.call(arguments, 1);
+    sources.forEach(function (source) {
+        for (var prop in source) {
+            target[prop] = source[prop];
+        }
+    });
+    return target;
+}
+
+module.exports = {
+	extend: extend
+}
