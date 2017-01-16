3990271329f56059d1d67c7095da43074fceac2f
nokizorque <noki@zorque.xyz>
2017-01-16 23:09:40 +1000

    Added an empty cache.js model

---
+++ models/cache.js
@@ -0,0 +1,8 @@
+/*
+// Copyright (C) 2016 - 2017 Lewis Watson <noki@zorque.xyz>
+//
+// This file is part of Twenti
+//
+// Twenti can not be copied and/or distributed without the express
+// permission of Lewis Watson and/or Franco Paez
+*/
