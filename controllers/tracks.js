7a8ed73924e20009a4cd62d7769085b51a3bd602
nokizorque <noki@zorque.xyz>
2017-01-16 23:08:45 +1000

    Refactored some parts to run parallel
    uniqueid -> trackid
    Included a JSON helper file
    Modified some indentation

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -7,21 +7,6 @@
 // permission of Lewis Watson and/or Franco Paez
 */
 
-var Track = {}
-var postgre = require("../models/postgre.js");
-var db = postgre.db;
-var pgp = postgre.pgp;
-var qrm = pgp.queryResult;
-
-var columns = {
-	"main": ["uniqueid", "title", "description", "artists", "uploaded"],
-	"statistics": [""]
-};
-
-Track.getSingle = function(req, res) {
-	var id = req.params.id;
-	db.one("select ${columns^} from tracks where uniqueid = ${uniqueid}", {uniqueid: id, columns: columns.main.map(pgp.as.name).join()})
-		.then(function(data) {
 			// Idea
 				// Currently integer fields return as string in the json response
 				// Loop through all fields
@@ -34,29 +19,79 @@ Track.getSingle = function(req, res) {
 				*/
 				// Should see if it's able to be made into a one-liner
 			// eg: data.uploaded = parseInt(data.uploaded);
-			res.status(200)
-				.json(
-					{
-						"response": {
-							"data": {
-								"tracks": [
-									data
-								]
-							}
-						}
-					}
-				);
-		})
-		.catch(function(err) {
-			console.log(err);
-			res.json(
-				{
-					"response": {
-						"data": null
+
+var Track 		= {}
+var postgre 	= require("../models/postgre.js");
+var db			= postgre.db;
+var pgp 		= postgre.pgp;
+var qrm 		= pgp.queryResult;
+var async		= require("async");
+var _json 		= require("../helpers/json.js");
+
+var columns = {
+	"main": ["trackid", "title", "description", "uploaded", "artists"]
+};
+
+Track.getSingle = function(req, res) {
+	var id = req.params.id;
+
+	async.parallel({
+		main: function(callback) {
+			setTimeout(function () {
+				db.one("select ${columns^} from tracks where trackid = ${trackid}", {trackid: id, columns: columns.main.map(pgp.as.name).join()})
+					.then(function(data) {
+						callback(null, data);
+					})
+					.catch(function(err) {
+						callback(err, null);
+					})
+			});
+		},
+		plays: function(callback) {
+			setTimeout(function () {
+				db.one("select coalesce(count(*), 0) as plays from tracks__plays where trackid = ${trackid}", {trackid: id})
+					.then(function(data) {
+						callback(null, data);
+					})
+					.catch(function(err) {
+						callback(err, null);
+					})
+			});
+		},
+		views: function(callback) {
+			setTimeout(function () {
+				// Must change this to some other sort of table
+				db.one("select coalesce(count(*), 0) as views from tracks__plays where trackid = ${trackid}", {trackid: id})
+					.then(function(data) {
+						callback(null, data);
+					})
+					.catch(function(err) {
+						callback(err, null);
+					})
+			});
+		}
+	},
+	function(err, results) {
+		if (err) {
+			res.json({message: "error"});
+			return;
+		}
+
+		// Needed to merge statistics into the main array
+		results.stats = {"statistics": _json.extend({}, results.views, results.plays)};
+
+		res.json(
+			{
+				"response": {
+					"data": {
+						"tracks": [
+							_json.extend({}, results.main, results.stats)
+						]
 					}
 				}
-			);
-		});
+			}
+		);
+	});
 }
 
 Track.getMany = function(req, res) {
@@ -77,10 +112,10 @@ Track.getMany = function(req, res) {
 
 	// https://github.com/vitaly-t/pg-promise/wiki/Learn-by-Example#where-col-in-values
 	// https://github.com/vitaly-t/pg-promise/issues/267
-	//db.query("select ${columns^} from tracks where uniqueid in (" + pgp.as.format("$1^", pgp.as.csv(ids)) + ")", {columns: columns.map(pgp.as.name).join()}, qrm.any)
+	//db.query("select ${columns^} from tracks where trackid in (" + pgp.as.format("$1^", pgp.as.csv(ids)) + ")", {columns: columns.map(pgp.as.name).join()}, qrm.any)
 
 	// Main query
-	db.query("select ${columns^} from tracks where uniqueid in (${uniqueids:csv})", {uniqueids: ids, columns: columns.main.map(pgp.as.name).join()}, qrm.any)
+	db.query("select ${columns^} from tracks where trackid in (${trackid:csv})", {trackid: ids, columns: columns.main.map(pgp.as.name).join()}, qrm.any)
 		.then(function(data) {
 			res.json(
 				{
@@ -104,7 +139,7 @@ Track.getMany = function(req, res) {
 		});
 
 	// Statistics
-	db.query()
+	//db.query()
 }
 
 Track.getAll = function(req, res) {
