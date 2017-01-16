744c30b3954bcc66ed8423debe5fd9bb11294272
nokizorque <noki@zorque.xyz>
2017-01-16 21:38:08 +1000

    Appended licensing headers to the tops of a few files

--- controllers/users.js
+++ controllers/users.js
@@ -1,1 +1,10 @@
+/*
+// Copyright (C) 2016 - 2017 Lewis Watson <noki@zorque.xyz>
+//
+// This file is part of Twenti
+//
+// Twenti can not be copied and/or distributed without the express
+// permission of Lewis Watson and/or Franco Paez
+*/
+
 var db = require("../models/postgre.js");
--- helpers/array.js
+++ helpers/array.js
@@ -1,3 +1,12 @@
+/*
+// Copyright (C) 2016 - 2017 Lewis Watson <noki@zorque.xyz>
+//
+// This file is part of Twenti
+//
+// Twenti can not be copied and/or distributed without the express
+// permission of Lewis Watson and/or Franco Paez
+*/
+
 module.exports.removeDuplicates = function(a) {
    return Array.from(new Set(a));
 }
