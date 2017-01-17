abee84c92e7f6f98b9a00a1caddd7cdabb61c5a0
nokizorque <noki@zorque.xyz>
2017-01-17 17:56:11 +1000

    Tracks.getMany now takes uses parallels & caching
    Fetches all data that is cached, and fetches for ones that aren't.
    Caches tracks that have been fetched, but does not recache already cached tracks.
    Needs inclusion of more async functionality.
    Query times down from ~60ms to ~15ms for two tracks.
    Needs testing with more tracks.

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -47,6 +47,7 @@ Track.getSingle = function(req, res) {
 				// Tracks use 'track:id.type' syntax in the cache, where type is 'main', 'plays' or 'views'
 				__cache.get("track:" + id + ".main", function(err, value) {
 					if (err) {
+						//console.log("Could not get cache for .main");
 						db.one("select ${columns^} from tracks where trackid = ${trackid}", {trackid: id, columns: columns.main.map(pgp.as.name).join()})
 							.then(function(data) {
 								callback(null, data);
@@ -56,6 +57,7 @@ Track.getSingle = function(req, res) {
 							})
 					}
 					else {
+						console.log("Using cache for .main");
 						callback(null, value);
 					}
 				});
@@ -65,6 +67,7 @@ Track.getSingle = function(req, res) {
 			setTimeout(function () {
 				__cache.get("track:" + id + ".stats", function (err, value) {
 					if (err) {
+						//console.log("Could not get cache for .stats");
 						db.one(
 							"SELECT (" + 
 								"SELECT COALESCE(COUNT(*), 0) " + 
@@ -85,6 +88,7 @@ Track.getSingle = function(req, res) {
 							})
 					}
 					else {
+						console.log("Using cache for .stats");
 						callback(null, value);
 					}
 				})
@@ -147,42 +151,102 @@ Track.getMany = function(req, res) {
 		}
 	}
 
-	// START DEBUG
-	//console.log(ids);
-	//console.log(columns.map(pgp.as.name).join());
-	//console.log(pgp.as.format("$1^", pgp.as.csv(ids)));
-	// END DEBUG
-
 	// https://github.com/vitaly-t/pg-promise/wiki/Learn-by-Example#where-col-in-values
 	// https://github.com/vitaly-t/pg-promise/issues/267
 	//db.query("select ${columns^} from tracks where trackid in (" + pgp.as.format("$1^", pgp.as.csv(ids)) + ")", {columns: columns.map(pgp.as.name).join()}, qrm.any)
 
-	// Main query
-	db.query("select ${columns^} from tracks where trackid in (${trackid:csv})", {trackid: ids, columns: columns.main.map(pgp.as.name).join()}, qrm.any)
-		.then(function(data) {
-			res.json(
-				{
-					"response": {
-						"data": {
-							"tracks": data // The JSON array [] is part of 'data'
-						}
-					}
-				}
-			);
-		})
-		.catch(function(err) {
-			console.log(err);
-			res.json(
-				{
-					"response": {
-						"data": err
-					}
-				}
-			);
-		});
+	async.parallel({
+		// Retrieve ones that are cached and query for ones that aren't cached
+		main: function(callback) {
+			setTimeout(function () {
+				// The aim in this function is to send all the results at once
 
-	// Statistics
-	//db.query()
+				var preResults = []; // Results we already have
+				var toFetch = []; // Ones that need to be fetched
+
+				// Cannot use .mget
+				for (i = 0; i < ids.length; i++) {
+					__cache.get("track:" + ids[i] + ".main", function (err, value) {
+						if (err) {
+							toFetch.push(ids[i]);
+						}
+						else {
+							preResults.push(value);
+						}
+					});
+				}
+
+				if (toFetch.length > 0) {
+					db.query("select ${columns^} from tracks where trackid in (${trackid:csv})", {trackid: toFetch, columns: columns.main.map(pgp.as.name).join()}, qrm.many)
+						.then(function(data) {
+							// We need to push each as an object and not as an array
+							/*
+								example:
+									[{"trackid": ...}] -> is not okay
+									{"trackid": ...} -> is okay
+							*/
+							if (data.length > 1) {
+								for (i = 0; i < data.length; i++) {
+									preResults.push(data[i]);
+								}
+							}
+							else {
+								preResults.push(data[0]);
+							}
+							callback(null, preResults);
+						})
+						.catch(function(err) {
+							callback(err, null);
+						});
+				}
+				else {
+					// All cached data has been pushed to preResults and we don't need to worry about fetching anything - just send it
+					callback(null, preResults);
+				}
+			});
+		}/*,
+		stats: function(callback) {
+			setTimeout(function () {
+			});
+		}
+		*/
+	},
+	function(err, results) {
+		if (err) {
+			res.json({message: "error"});
+			return;
+		}
+
+		var tracksArray = results.main;
+		for (i = 0; i < tracksArray.length; i++) {
+			if (tracksArray[i].trackid == undefined) {
+				console.log(tracksArray);
+			}
+			console.log("Going through " + tracksArray[i].trackid);
+			__cache.get("track:" + tracksArray[i].trackid + ".main", function(_err, value) {
+				if (_err) {
+					__cache.set("track:" + tracksArray[i].trackid + ".main", tracksArray[i], function (err, success) {
+						if (err) {
+							console.log("Failed to update cache for " + tracksArray[i].trackid);
+						}
+						else {
+							console.log("Added cache for " + tracksArray[i].trackid);
+						}
+					});
+				}
+			});
+		}
+
+		res.json(
+			{
+				"response": {
+					"data": {
+						tracks: tracksArray
+					}
+				}
+			}
+		);
+	});
 }
 
 Track.getAll = function(req, res) {
