ce2e4e3018a4821517d093fe0b2054f6becf54ce
nokizorque <noki@zorque.xyz>
2017-01-17 15:10:45 +1000

    Made cache.js model functional

--- models/cache.js
+++ models/cache.js
@@ -6,3 +6,21 @@
 // Twenti can not be copied and/or distributed without the express
 // permission of Lewis Watson and/or Franco Paez
 */
+
+// https://www.npmjs.com/package/node-cache
+
+// Constants
+const CACHE_CHECK_PERIOD 			= 300; 		// 300s = 5m
+const CACHE_DEFAULT_TTL 			= 1800; 	// 1800s = 30m
+const CACHE_THROW_ERR_MISSING		= true; 	// true = throw an error when unable to retrieve key, false = return undefined instead
+const CACHE_RETURN_COPY 			= false; 	// true = return copy of variable, false = return reference of variable
+
+const NodeCache = require("node-cache");
+const __cache = new NodeCache({
+	stdTTL: 			CACHE_DEFAULT_TTL,
+	checkperiod: 		CACHE_CHECK_PERIOD,
+	errorOnMissing: 	CACHE_THROW_ERR_MISSING,
+	useClones: 			CACHE_RETURN_COPY
+});
+
+module.exports = __cache;
